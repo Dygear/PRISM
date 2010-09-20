e01bc69eca40ae448e7be7f937a3e99889c602dd
Victor van Vlaardingen <vic@lfs.net>
2010-09-20 18:55:19 +0200

    Telnet with basic DOM model to draw stuff onto the screen. Text styles already work (for the most part I guess)

--- modules/prism_telnet_defines.php
+++ modules/prism_telnet_defines.php
@@ -100,12 +100,43 @@ define('TELNET_MODE_NAWS', 16);
 define('TELNET_MODE_INSERT', 1024);
 define('TELNET_MODE_LINEEDIT', 2048);
 
-// Some control character defines - saves us from having to do ord() on the characters all the time
-define('KEY_IP',					chr(0x03));			// backspace
+define('TELNET_ECHO_NORMAL', 1);
+define('TELNET_ECHO_STAR', 2);
+define('TELNET_ECHO_NOTHING', 3);
+
+define('TELNET_CURSOR_HIDE', 1);
+
+// Standard control keys
+define('KEY_IP',					chr(0x03));			// Interrupt Process (break)
 define('KEY_BS',					chr(0x08));			// backspace
 define('KEY_TAB',					chr(0x09));			// TAB
+define('KEY_ENTER',					chr(0x0A));			// Enter
 define('KEY_ESCAPE',				chr(0x1B));			// escape
 define('KEY_DELETE',				chr(0x7F));			// del
+
+// Self defined key codes
+define('KEY_CURLEFT',				chr(0x01).chr(0));		// Cursor LEFT
+define('KEY_CURRIGHT',				chr(0x01).chr(1));		// Cursor LEFT
+define('KEY_CURUP',					chr(0x01).chr(2));		// Cursor LEFT
+define('KEY_CURDOWN',				chr(0x01).chr(3));		// Cursor LEFT
+define('KEY_HOME',					chr(0x01).chr(4));		// Home
+define('KEY_END',					chr(0x01).chr(5));		// End
+define('KEY_PAGEUP',				chr(0x01).chr(6));		// Home
+define('KEY_PAGEDOWN',				chr(0x01).chr(7));		// End
+define('KEY_INSERT',				chr(0x01).chr(8));		// Insert
+
+define('KEY_F1',					chr(0x01).chr(11));		// F1
+define('KEY_F2',					chr(0x01).chr(12));		// F2
+define('KEY_F3',					chr(0x01).chr(13));		// F3
+define('KEY_F4',					chr(0x01).chr(14));		// F4
+define('KEY_F5',					chr(0x01).chr(15));		// F5
+define('KEY_F6',					chr(0x01).chr(17));		// F6
+define('KEY_F7',					chr(0x01).chr(18));		// F7
+define('KEY_F8',					chr(0x01).chr(19));		// F8
+define('KEY_F9',					chr(0x01).chr(20));		// F9
+define('KEY_F10',					chr(0x01).chr(21));		// F10
+define('KEY_F11',					chr(0x01).chr(23));		// F11
+define('KEY_F12',					chr(0x01).chr(24));		// F12
 
 // ANSI escape sequences VT100
 define('VT100_USG0',				KEY_ESCAPE.'(B');
@@ -120,24 +151,34 @@ define('VT100_G1_ALTROM_GFX',		KEY_ESCAPE.')2');
 define('VT100_SSHIFT2',				KEY_ESCAPE.'N');
 define('VT100_SSHIFT3',				KEY_ESCAPE.'O');
 
-define('VT100_ED2',					KEY_ESCAPE.'[2J');
+define('VT100_ED2',					KEY_ESCAPE.'[2J');		// Clear entire screen
 
-define('VT100_CURSORHOME',			KEY_ESCAPE.'[H');
+define('VT100_CURSORHOME',			KEY_ESCAPE.'[H');		// Move cursor to upper-left corner
 
-define('VT100_STYLE_RESET',			KEY_ESCAPE.'[m');		// Attribs off
+define('VT100_STYLE_RESET',			KEY_ESCAPE.'[0m');		// Attribs off
 define('VT100_STYLE_BOLD',			KEY_ESCAPE.'[1m');		// bold
-define('VT100_SGR2',				KEY_ESCAPE.'[2m');		// low intensity
-define('VT100_SGR4',				KEY_ESCAPE.'[4m');		// underline
-define('VT100_SGR5',				KEY_ESCAPE.'[5m');		// blink
-define('VT100_SGR7',				KEY_ESCAPE.'[7m');		// reverse video
-define('VT100_SGR8',				KEY_ESCAPE.'[8m');		// invisible text
+define('VT100_STYLE_LOWINTENS',		KEY_ESCAPE.'[2m');		// low intensity
+define('VT100_STYLE_UNDERLINE',		KEY_ESCAPE.'[4m');		// underline
+define('VT100_STYLE_BLINK',			KEY_ESCAPE.'[5m');		// blink
+define('VT100_STYLE_REVERSE',		KEY_ESCAPE.'[7m');		// reverse video
+define('VT100_STYLE_INVISIBLE',		KEY_ESCAPE.'[8m');		// invisible text
 
-define('VT100_DECLL0',				KEY_ESCAPE.'[0q');		// 
-define('VT100_DECLL1',				KEY_ESCAPE.'[1q');		// 
-define('VT100_DECLL2',				KEY_ESCAPE.'[2q');		// 
-define('VT100_DECLL3',				KEY_ESCAPE.'[3q');		// 
-define('VT100_DECLL4',				KEY_ESCAPE.'[4q');		// 
+define('VT100_STYLE_BLACK',			KEY_ESCAPE.'[30m');
+define('VT100_STYLE_RED',			KEY_ESCAPE.'[31m');
+define('VT100_STYLE_GREEN',			KEY_ESCAPE.'[32m');
+define('VT100_STYLE_YELLOW',		KEY_ESCAPE.'[33m');
+define('VT100_STYLE_BLUE',			KEY_ESCAPE.'[34m');
+define('VT100_STYLE_MAGENTA',		KEY_ESCAPE.'[35m');
+define('VT100_STYLE_CYAN',			KEY_ESCAPE.'[36m');
+define('VT100_STYLE_WHITE',			KEY_ESCAPE.'[37m');
 
-//define('VT100_', KEY_ESCAPE.'');
+define('VT100_STYLE_BG_BLACK',		KEY_ESCAPE.'[40m');
+define('VT100_STYLE_BG_RED',		KEY_ESCAPE.'[41m');
+define('VT100_STYLE_BG_GREEN',		KEY_ESCAPE.'[42m');
+define('VT100_STYLE_BG_YELLOW',		KEY_ESCAPE.'[43m');
+define('VT100_STYLE_BG_BLUE',		KEY_ESCAPE.'[44m');
+define('VT100_STYLE_BG_MAGENTA',	KEY_ESCAPE.'[45m');
+define('VT100_STYLE_BG_CYAN',		KEY_ESCAPE.'[46m');
+define('VT100_STYLE_BG_WHITE',		KEY_ESCAPE.'[47m');
 
 ?>
