aa637bcedaa1c919bb7c606f7d0e47416567596d
Dygear <Dygear@gmail.com>
2011-07-01 12:08:02 -0400

    Updated pylons to use simple packet syntax.

--- plugins/pylons.php
+++ plugins/pylons.php
@@ -4,38 +4,77 @@ class pylons extends Plugins
 	const URL = '..';
 	const NAME = 'Pylons AutoX Objects Demo';
 	const AUTHOR = "Kai Lochbaum aka GeForz";
-	const VERSION = 'demo';
-	const DESCRIPTION = 'Paints Pylons!';
+	const VERSION = 'Demo';
+	const DESCRIPTION = 'Paints player\'s direction on the track';
 	
 	private $Pylons = array();
 	private $PylonList = array();
+	private $canPlace = TRUE;
 	
 	public function __construct()
 	{
+		$this->registerSayCommand('pylons free', 'cmdFree', 'Frees all AutoX Objects', ADMIN_OBJECT);
 		$this->registerPacket('onPrismConnect', ISP_VER);
 		$this->registerPacket('onMCI', ISP_MCI);
 		$this->registerPacket('onObjectHit', ISP_OBH);
 	}
 	
+	public function cmdFree($cmd, $ucid)
+	{
+		$AXM = IS_AXM()->PMOAction(PMO_CLEAR_ALL)->send();
+
+/*
+		if (count($this->PylonList) == 0)
+			return PLUGIN_HANDLED;
+
+		while (count($this->PylonList) > 0)
+		{
+			$obsolete = array_slice($this->PylonList, 0, 30);
+			$this->PylonList = array_values(array_slice($this->PylonList, 30));
+			
+			$obsolete_items = array();
+			foreach ($obsolete as $obskey)
+			{
+				$obsolete_items[] = $this->Pylons[$obskey];
+				unset($this->Pylons[$obskey]);
+			}
+			
+			$AXM = new IS_AXM();
+			$AXM->PMOAction(PMO_DEL_OBJECTS)->Info($obsolete_items)->send();
+		}
+*/
+	}
+	
 	public function onPrismConnect(IS_VER $VER)
 	{
-		$AXM = new IS_AXM();
-		$AXM->PMOAction(PMO_CLEAR_ALL)->send();
+		IS_AXM()->PMOAction(PMO_CLEAR_ALL)->send();
 	}
 
-	
+	public function onAutoX(IS_AXM $AXM)
+	{
+		$this->canPlace = TRUE;
+		foreach ($AXM->Info as $Object)
+		{
+			$Key = $Object->rawX().':'.$Object->rawY();
+			$this->PylonList[] = $Key;
+			$this->Pylons[$Key] = $Object;
+		}
+	}
+
 	public function onMCI(IS_MCI $MCI)
 	{
-		// drop pylon on mci place :O
+		if ($this->canPlace !== TRUE)
+			return PLUGIN_CONTINUE;
+		
+		$this->canPlace = FALSE;
+		
 		foreach($MCI->Info as $Info)
 		{
 			$x = $Info->X/65536;
 			$y = $Info->Y/65536;
 			$obj = new LayoutObject($x, $y, $Info->Z/65536, LayoutObject::$OBJ_REAL_OBJECT, 180 + $Info->Heading/32768 * 180, LayoutObject::$AXO_CHALK_AHEAD, ($Info->PLID % 4));
-			
-			// THIS IS BAD! Listen for new added object packet from lfs and use those...
-			$key = $obj->rawX().':'.$obj->rawY();
-			
+			$key = "{$obj->rawX()}:{$obj->rawY()}";
+
 			for($i = 0; $i < count($this->PylonList); $i++)
 			{
 				if ($this->PylonList[$i] == $key)
@@ -45,32 +84,12 @@ class pylons extends Plugins
 					break;
 				}
 			}
-			$this->PylonList[] = $key;
-			$this->Pylons[$key] = $obj;
-			
-			/*
-			if (count($this->PylonList) > 700)
-			{
-				$obsolete = array_slice($this->PylonList, 0, 30);
-				$this->PylonList = array_values(array_slice($this->PylonList, 30));
-				
-				$obsolete_items = array();
-				foreach ($obsolete as $obskey)
-				{
-					$obsolete_items[] = $this->Pylons[$obskey];
-					unset($this->Pylons[$obskey]);
-				}
-				
-				$AXM = new IS_AXM();
-				$AXM->PMOAction(PMO_DEL_OBJECTS)->Info($obsolete_items)->send();
-			}
-			*/
-			
-			if (count($this->PylonList) < 500)
+						
+			if (count($this->PylonList) < 800)
 			{
 				$AXM = new IS_AXM();
 				$AXM->PMOAction(PMO_ADD_OBJECTS)->Info(array($obj))->send();
-				echo "New Object at " . $key . "\n";
+				console("New Object at {$key}");
 			}
 		}
 	}
@@ -83,14 +102,13 @@ class pylons extends Plugins
 			return;
 		}
 		
-		$key = $OBH->X.':'.$OBH->Y;
+		$key = "{$OBH->X}:{$OBH->Y}";
 		
 		if (isset($this->Pylons[$key]))
 		{
 			// move object out of the way
 			$obj = $this->Pylons[$key];
-			$AXM = new IS_AXM();
-			$AXM->PMOAction(PMO_DEL_OBJECTS)->Info(array($obj))->send();
+			IS_AXM()->PMOAction(PMO_DEL_OBJECTS)->Info(array($obj))->send();
 			unset($this->Pylons[$key]);
 			
 			$xyh = new XYHelper($OBH->C->Heading / 128 * 180);
@@ -102,7 +120,7 @@ class pylons extends Plugins
 		}
 		else
 		{
-			echo "unknown object at: " . $key;
+			console("Unknown object at: {$key}.");
 		}
 	}
 }
