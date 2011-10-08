d20a6ba1fa3cf18b132ccf460a81d61426db952c
Dygear <Dygear@gmail.com>
2011-10-08 09:47:30 -0400

    Updated the LVS plugin to use the new PTH parser. Still not stable. Don't use, will crash your PRISM instance.

--- plugins/LVS.php
+++ plugins/LVS.php
@@ -3,88 +3,54 @@ class LVS extends Plugins
 {
 	const URL = 'http://lfsforum.net/forumdisplay.php?f=312';
 	const NAME = 'LVS';
-	const AUTHOR = 'PRISM Dev Team';
+	const AUTHOR = 'PRISM Dev Team & avetere';
 	const VERSION = PHPInSimMod::VERSION;
 	const DESCRIPTION = 'Lap Verification System.';
 
-	private $LVS = FALSE;
-	private $Path = array();
-	private $Track = '';
+	private $pathPoly = array();
+	private $onTrack = array();
 
 	public function __construct()
 	{
-		$this->registerSayCommand('prism lvs debug', 'cmdDebug', 'Prints Debug Information', ADMIN_ALL);
-		$this->registerSayCommand('prism lvs', 'cmdLVSToggle', '<On|Off> - Turns Lap Verification System On / Off', ADMIN_CVAR + ADMIN_TRACK);
-		$this->registerPacket('onTrack', ISP_STA, ISP_RST);
+		$this->registerPacket('onTrack', ISP_STA);
 		$this->registerPacket('onMCI', ISP_MCI);
 	}
 
-	public function cmdDebug($cmd, $ucid)
+	public function onTrack(IS_STA $STA)
 	{
-		$MTC = IS_MTC()->UCID($ucid);
-
-		$debug = explode(PHP_EOL, print_r($this, TRUE));
-		foreach ($debug as $line)
-			$MTC->Text($line)->Send();
-	}
-
-	public function cmdLVSToggle($cmd, $ucid)
-	{
-		$MTC = IS_MTC()->UCID($ucid);
-		if (($argc = count($argv = str_getcsv($cmd, ' '))) > 2)
-		{
-			$OnOff = strtolower($argv[2]);
-			if ($OnOff == 'on')
-			{
-				$this->LVS = TRUE;
-				$MTC->Text('Lap Verification System is now ^3On^8!');
-			}
-			else if ($OnOff == 'off')
-			{
-				$this->LVS = FALSE;
-				$MTC->Text('Lap Verification System is now ^3Off^8!');
-			}
-			else
-				$MTC->Text('Please provide a ^3On^8 or ^3Off^8 only as an argument to this command.');
-		}
-		else
-			$MTC->Text('Lap Verification System is currently ^3' . (($this->LVS) ? 'On' : 'Off') . '^8.');
-		$MTC->Send();
-	}
-	
-	public function onTrack(Struct $Packet)
-	{
-		if ($this->Track == $Packet->Track)
+		static $Track;
+		if ($Track == $STA->Track)
 			return PLUGIN_CONTINUE;
 		else
-			$this->Track = $Packet->Track;
+			$Track = $STA->Track;
 
-		$file = "../data/pth/{$this->Track}.pth";
+		$file = ROOTPATH . '/data/pth/' . $Track . '.pth';
+
 		if (!file_exists($file))
 			return PLUGIN_CONTINUE;
 
-		$pth = new pth($file);
-		print_r($pth);
-		$this->Path = array();
-		foreach ($pth->Nodes as $Node)
-			$this->Path[] = $Node->toPolyRoad();
+		$pth = new PTH($file);
+		$this->pathPoly = array();
+		foreach ($pth->Nodes as $NodeID => $Node)
+			$this->pathPoly[$NodeID] = $Node->toPointRoad();
+		
+		print_r($this->pathPoly);
+		
+		console('Read track path file successfully.');
 	}
 	
 	public function onMCI(IS_MCI $MCI)
 	{
-		if ($this->LVS === FALSE)
-			return PLUGIN_CONTINUE;
-
 		foreach ($MCI->Info as $Info)
 		{
-			if ($this->OffTrack[$Info->PLID] !== TRUE AND !$this->isInPoly($Info->X, $Info->Y, $this->Path[$Info->Node]))
-			{
-				$this->OffTrack[$Info->PLID] = TRUE; # So we don't spam the client.
-				IS_MTC()->UCID($this->getClientByPLID($Info->PLID)->UCID)->Text('You are not on the track!')->Send();
-			} else if ($this->OffTrack[$Info->PLID]) {
-				$this->OffTrack[$Info->PLID] = FALSE; # Set the car as being on the track once again!
-			}
+			if ($this->inPoly($Info->X, $Info->Y, $this->pathPoly))
+				$Text = '^4On Track^9';
+			else
+				$Text = '^1Off Track^9';
+
+			IS_MTC()->PLID($Info->PLID)->Text("{$Text} - {$Info->Node}: {$Info->X}, {$Info->Y}")->Send();
 		}
+		exit();
 	}
 }
 ?>
