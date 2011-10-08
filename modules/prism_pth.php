72c10315f048b252ed0bd4c192f2c114885b8aca
Dygear <Dygear@gmail.com>
2011-10-08 09:46:39 -0400

    Completly rewrote the PTH parer.

--- modules/prism_pth.php
+++ modules/prism_pth.php
@@ -4,83 +4,124 @@
  * @package PRISM
  * @subpackage PTH
 */
-
+// PaTH
 class PTH
 {
-	const HEADER = 'CVersion/CRevision/lNodes/lFinishLine';
+	const PACK = 'a6CCll';
+	const UNPACK = 'a6LFSPTH/CVersion/CRevision/lNumNodes/lFinishLine';
 
-	public $Version;
-	public $Revision;
-	public $Nodes;
+	public $LFSPTH = 'LFSPTH';
+	public $Version = 0;
+	public $Revision = 0;
+	public $NumNodes;
 	public $FinishLine;
+	public $Nodes = array();
 
 	public function __construct($pthFilePath)
 	{
-		$this->file = file_get_contents($pthFilePath);
-		$this->readHeader($this->file);
-		for ($Node = 0; $Node < $this->Nodes; ++$Node)
-			$this->Node[$Node] = $this->readNode($Node);
-		unset($this->file);
+		$file = file_get_contents($pthFilePath);
+
+		if ($this->unPack($file) === TRUE)
+			return; # trigger_error returns (bool) TRUE, so if the return is true, there was an error.
+
+		return $this;
 	}
-	protected function readHeader()
+	public function unPack($file)
 	{
-		foreach (unpack(PTH::HEADER, substr($this->file, 6, 10)) as $property => $value)
+		if (substr($file, 0, 6) != $this->LFSPTH)
+			return trigger_error('Not a LFS PTH file', E_USER_ERROR);
+
+		if (substr($file, 6, 1) != $this->Version)
+			return trigger_error('Not a LFS PTH Verion Is Different Than PTH Paser', E_USER_ERROR);
+
+		if (substr($file, 7, 1) != $this->Revision)
+			return trigger_error('Not a LFS PTH Revision Is Different Than PTH Paser', E_USER_ERROR);
+		
+		foreach (unpack(self::UNPACK, substr($file, 0, 16)) as $property => $value)
 			$this->$property = $value;
-	}
-	protected function readNode($Node)
-	{
-		$RawNode = substr($this->file, 16 + ($Node * 40), 40);
-		$NodeObj = new Node;
-		$NodeObj->readCenter($RawNode);
-		$NodeObj->readDirection($RawNode);
-		$NodeObj->readLimit($RawNode);
-		$NodeObj->readRoad($RawNode);
-		return $NodeObj;
+
+		for ($Node = 0; $Node < $this->NumNodes; ++$Node)
+			$this->Nodes[$Node] = new Node(substr($file, 16 + ($Node * 40), 40));
+
+		return $this;
 	}
 }
 class Node
 {
-	const CENTER = 'lX/lY/lZ';
-	const DIRECTION = 'fX/fY/fZ';
-	const LIMIT = 'fLeft/fRight';
-	const ROAD = 'fLeft/fRight';
-
 	public $Center;
 	public $Direction;
 	public $Limit;
 	public $Road;
 
-	public function readCenter($RawNode)
-	{
-		$this->Center = (object) unpack(Node::CENTER, substr($RawNode, 0, 12));
+	public function __construct($RawNode) {
+		$this->Center = new Center(substr($RawNode, 0, 12));
+		$this->Direction = new Direction(substr($RawNode, 12, 12));
+		$this->Limit = new Limit(substr($RawNode, 24, 8), $this);
+		$this->Road = new Road(substr($RawNode, 32, 8), $this);
 	}
-	public function readDirection($RawNode)
-	{
-		$this->Direction = (object) unpack(Node::DIRECTION, substr($RawNode, 12, 12));
+}
+class Center
+{
+	const PACK = 'lll';
+	const UNPACK = 'lX/lY/lZ';
+	
+	public function __construct($rawData) {
+		$this->unPack($rawData);
 	}
-	public function readLimit($RawNode)
-	{
-		$this->Limit = (object) unpack(Node::LIMIT, substr($RawNode, 24, 8));
+	public function unPack($rawData) {
+		foreach (unpack(self::UNPACK, $rawData) as $property => $value)
+			$this->$property = $value;
 	}
-	public function readRoad($RawNode)
-	{
-		$this->Road = (object) unpack(Node::ROAD, substr($RawNode, 32, 8));
+}
+class Direction
+{
+	const PACK = 'fff';
+	const UNPACK = 'fX/fY/fZ';
+
+	public function __construct($rawData) {
+		$this->unPack($rawData);
 	}
-	public function toPolyLimit()
-	{
-		$XL = $this->Center->X + $this->Limit->Left;
-		$XR = $this->Cetner->X + $this->Limit->Right;
-		$YL = $this->Center->Y + $this->Limit->Left;
-		$YR = $this->Cetner->Y + $this->Limit->Right;
-		return array($XL, $XR, $YL, $YR);
+	public function unPack($rawData) {
+		foreach (unpack(Center::UNPACK, $rawData) as $property => $value)
+			$this->$property = $value;
 	}
-	public function toPolyRoad()
-	{
-		$XL = $this->Center->X + $this->Road->Left;
-		$XR = $this->Cetner->X + $this->Road->Right;
-		$YL = $this->Center->Y + $this->Road->Left;
-		$YR = $this->Cetner->Y + $this->Road->Right;
-		return array($XL, $XR, $YL, $YR);
+}
+class Limit extends PointDetail
+{
+	const PACK = 'ff';
+	const UNPACK = 'fLeft/fRight';
+}
+class Road extends PointDetail
+{
+	const PACK = 'ff';
+	const UNPACK = 'fLeft/fRight';
+}
+abstract class PointDetail
+{
+	protected $parent;
+	
+	public function __construct($rawData, $parent) {
+		$this->unPack($rawData);
+		$this->parent = $parent;
+	}
+
+	public function pointLeft() {
+		$Direction =& $this->parent->Direction;
+		$Center =& $this->parent->Center;
+		$cosTan = cos(atan2($Direction->X, $Direction->Y));
+		$sinTan = sin(atan2($Direction->X, $Direction->Y));
+		return new Point($Center->X + $this->Left * $cosTan, $Center->Y - $this->Left * $sinTan);
+	}
+	public function pointRight() {
+		$Direction =& $this->parent->Direction;
+		$Center =& $this->parent->Center;
+		$cosTan = cos(atan2($Direction->X, $Direction->Y));
+		$sinTan = sin(atan2($Direction->X, $Direction->Y));
+		return new Point($Center->X + $this->Right * $cosTan, $Center->Y - $this->Right * $sinTan);
+	}
+	public function unPack($rawData) {
+		foreach (unpack($this::UNPACK, $rawData) as $property => $value)
+			$this->$property = $value;
 	}
 }
 ?>
