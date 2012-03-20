bb19126d224b8d2011925014da051d4f09e0ae99
unknown <Povilas@Povilas-PC.(none)>
2012-03-20 20:09:04 +0000

    Fixed up timer bug, which caused another timer with same timestamp to be removed.

--- modules/prism_timers.php
+++ modules/prism_timers.php
@@ -5,13 +5,22 @@ class Timers
 	protected $timeout = NULL;		# When the next timeout is, read only from outside of this class.
 
 	// Registers a callback method.
-	protected function createTimer($callback, $interval = 1.0, $flags = Timer::CLOSE, $args = array())
-	{
-		# This will be the time when this timer is to trigger.
-		$timestamp = microtime(TRUE) + $interval;
-		# Adds our timer to the array.
-		$this->timers["$timestamp"] = new Timer($this, $callback, $interval, $flags, $args);
-	}
+	protected function createTimer($callback, $interval = 1.0, $flags = Timer::CLOSE, $args = array()) 
+    { 
+        # This will be the time when this timer is to trigger 
+        $timestamp = microtime(TRUE) + $interval; 
+         
+        # Check to make sure that another timer with same timestamp doesn't exist 
+        if (isset($this->timers["$timestamp"])) 
+        { 
+            $this->createTimer($callback, $interval, $flags, $args); 
+        } 
+        else  
+        { 
+            # Adds our timer to the array. 
+            $this->timers["$timestamp"] = new Timer($this, $callback, $interval, $flags, $args); 
+        } 
+    }  
 
 	// Sort the array to make sure the next timer (smallest float) is on the top of the list.
 	protected function sortTimers()
