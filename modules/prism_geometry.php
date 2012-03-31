88e7b87c86f2a375277ae8e94ec127207e142893
Victor van Vlaardingen <vic@lfs.net>
2012-03-31 06:29:32 +0200

    New drawPath method and Custom PTH support

--- modules/prism_geometry.php
+++ modules/prism_geometry.php
@@ -19,34 +19,162 @@ class Point2D
 
 class Polygon2D
 {
-    public $points      = array();
-    public $numPoints   = 0;
+    public $Points      = array();
+    public $NumPoints   = 0;
     
     public function __construct(array $points = array())
     {
-        $this->points       = $points;
-        $this->numPoints    = count($points);
+        $this->Points       = $points;
+        $this->NumPoints    = count($points);
     }
     
     public function __destruct()
     {
-        array_splice($this->points, 0, $this->numPoints);
+        array_splice($this->Points, 0, $this->NumPoints);
     }
     
-    public function contains(Point2D $point)
+    //  Simple check for convex polygons
+    public function contains(Point2D $p)
     {
-        // Simple check for convex poly
-        for ($i = 0; $i < $this->numPoints; $i++)
-        {
-            $j = ($i + 1) % $this->numPoints;
-            $p1 = $this->points[$i];
-            $p2 = $this->points[$j];
+        $p1 = $this->Points[0];
+        $p2 = $this->Points[1];
+        $p3 = $this->Points[2];
+        $p4 = $this->Points[3];
 
-            if (($point->y - $p1->y) * ($p2->x - $p1->x) - ($point->x - $p1->x) * ($p2->y - $p1->y) < 0)
-                return false;
+        if (($p->y - $p1->y) * ($p2->x - $p1->x) - ($p->x - $p1->x) * ($p2->y - $p1->y) >= 0 &&
+            ($p->y - $p3->y) * ($p4->x - $p3->x) - ($p->x - $p3->x) * ($p4->y - $p3->y) >= 0)
+        {
+            $p1 = $this->Points[1];
+            $p2 = $this->Points[2];
+            $p3 = $this->Points[3];
+            $p4 = $this->Points[0];
+
+            if (($p->y - $p1->y) * ($p2->x - $p1->x) - ($p->x - $p1->x) * ($p2->y - $p1->y) >= 0 &&
+                ($p->y - $p3->y) * ($p4->x - $p3->x) - ($p->x - $p3->x) * ($p4->y - $p3->y) >= 0)
+            {
+                return true;
+            }
+        }
+    }
+}
+
+// Simple polygon grid.
+// You give it a number of rows and columns and the radius of the grid in the constructor.
+// Polygons will be stored in all the grid blocks that they intersect with.
+// Easiest method to create a new PolyGrid is via the static method PolyGrid::fromArray (see below).
+// Or otherwise create a blank PolyGrid and use the insert method to add polygons one at a time.
+// You can then get the polygons in a certain block through the method getArrayByPoint (see below).
+class PolyGrid
+{
+    private $Radius;
+    private $Rows;
+    private $Cols;
+    private $RowHeight;
+    private $ColWidth;
+    private $Grid = array();
+    private $GridCounter = array();
+    
+    public function __construct($rows = 2, $cols = 2, $radius = 0x7FFFFFFF)
+    {
+        $this->Radius = $radius;
+        $this->Rows = $rows;
+        $this->Cols = $cols;
+        $this->RowHeight = ($this->Radius * 2) / $this->Cols;
+        $this->ColWidth = ($this->Radius * 2) / $this->Rows;
+        
+        // Create grid
+        for ($a = 0; $a < $rows; $a++)
+        {
+            $this->Grid[] = array();
+            $this->GridCounter[] = array();
+            for ($b = 0; $b < $cols; $b++)
+            {
+                $this->Grid[$a][] = array();
+                $this->GridCounter[$a][] = 0;
+            }
+        }
+    }
+    
+    public function insert(PolyGridData $data)
+    {
+        $q = array();
+        foreach ($data->Poly->Points as $point)
+        {
+            // find block in grid
+            $gi = $this->getGridIndexByPoint($point);
+            if (!in_array($data, $this->Grid[$gi[0]][$gi[1]]))
+            {
+                $this->Grid[$gi[0]][$gi[1]][] = $data;
+                $this->GridCounter[$gi[0]][$gi[1]]++;
+            }
+        }
+    }
+    
+    // $point - the point lying in the grid block we're looking for
+    // $polyArray - the found grid block will be written to this parameter
+    // Returns the number of polygons in the block or -1 if no grid block found
+    public function getArrayByPoint(Point2D $point, array &$polyArray)
+    {
+        $gi = $this->getGridIndexByPoint($point);
+        if ($gi !== null)
+        {
+            $polyArray = $this->Grid[$gi[0]][$gi[1]];
+            return $this->GridCounter[$gi[0]][$gi[1]];
+        }
+        return -1;
+    }
+    
+    private function getGridIndexByPoint(Point2D $point)
+    {
+        $x = -$this->Radius;
+        $y = -$this->Radius;
+        
+        for ($a = 0; $a < $this->Rows; $a++)
+        {
+            // find row
+            if ($point->y > $y && $point->y < $y + $this->RowHeight)
+            {
+                // Find column
+                for ($b = 0; $b < $this->Cols; $b++)
+                {
+                    if ($point->x > $x && $point->x < $x + $this->ColWidth)
+                    {
+                        return array($a, $b);
+                    }
+                    $x += $this->ColWidth;
+                }
+                
+                break;
+            }
+            $y += $this->RowHeight;
         }
         
-        return true;
+        return null;
+    }
+    
+    // Creates a new PolyGrid object and populates it with given array of polygons
+    public static function fromArray(array &$polygons, $rows = 2, $cols = 2, $radius = 0x7FFFFFFF)
+    {
+        $qt = new PolyGrid($rows, $cols, $radius);
+        
+        foreach ($polygons as $i => $poly)
+        {
+            $qt->insert(new PolyGridData($i, $poly));
+        }
+        
+        return $qt;
+    }
+}
+
+class PolyGridData
+{
+    public $Key;
+    public $Poly;
+    
+    public function __construct($key, Polygon2D $poly)
+    {
+        $this->Key = $key;
+        $this->Poly = $poly;
     }
 }
 
--- modules/prism_pth.php
+++ modules/prism_pth.php
@@ -8,55 +8,65 @@ require_once(ROOTPATH . '/modules/prism_geometry.php');
 // PaTH
 class PTH
 {
-	const PACK = 'a6CCll';
-	const UNPACK = 'a6LFSPTH/CVersion/CRevision/VNumNodes/VFinishLine';
+    const PACK = 'a6CCll';
+    const UNPACK = 'a6LFSPTH/CVersion/CRevision/VNumNodes/VFinishLine';
 
-	public $LFSPTH = 'LFSPTH';
-	public $Version = 0;
-	public $Revision = 0;
-	public $NumNodes;
-	public $FinishLine;
-	public $Nodes = array();
-	public $polyRoad = array();
-	public $polyLimit = array();
+    public $LFSPTH = 'LFSPTH';
+    public $Version = 0;
+    public $Revision = 0;
+    public $Custom = false;
+    public $NumNodes;
+    public $FinishLine;
+    public $Nodes = array();
+    public $polyRoad = array();
+    public $polyLimit = array();
+    public $polyRoadBlocks = null;
+    public $polyLimitBlocks = null;
 
-	public function __construct($pthFilePath)
-	{
-		$file = file_get_contents($pthFilePath);
+    public function __construct($pthFilePath, $custom = false)
+    {
+        $file = file_get_contents($pthFilePath);
+        $this->Custom = $custom;
 
-		if ($this->unPack($file) === TRUE)
-			return; # trigger_error returns (bool) TRUE, so if the return is true, there was an error.
-
-		return $this;
-	}
-	public function __destruct()
-	{
+        if ($this->unPack($file) === TRUE)
+            return; # trigger_error returns (bool) TRUE, so if the return is true, there was an error.
+        
+        return $this;
+    }
+    public function __destruct()
+    {
         array_splice($this->Nodes, 0, $this->NumNodes);
         array_splice($this->polyRoad, 0, $this->NumNodes);
         array_splice($this->polyLimit, 0, $this->NumNodes);
-	}
-	public function unPack($file)
-	{
-		if (substr($file, 0, 6) != $this->LFSPTH)
-			return trigger_error('Not a LFS PTH file', E_USER_ERROR);
+    }
+    public function unPack($file)
+    {
+        if (substr($file, 0, 6) != $this->LFSPTH)
+            return trigger_error('Not a LFS PTH file', E_USER_ERROR);
 
-		if (substr($file, 6, 1) != $this->Version)
-			return trigger_error('Not a LFS PTH Version Is Different Than PTH Parser', E_USER_ERROR);
+        if (substr($file, 6, 1) != $this->Version)
+            return trigger_error('Not a LFS PTH Version Is Different Than PTH Parser', E_USER_ERROR);
 
-		if (substr($file, 7, 1) != $this->Revision)
-			return trigger_error('Not a LFS PTH Revision Is Different Than PTH Parser', E_USER_ERROR);
-		
-		foreach (unpack(self::UNPACK, substr($file, 0, 16)) as $property => $value)
-			$this->$property = $value;
+        if (substr($file, 7, 1) != $this->Revision)
+            return trigger_error('Not a LFS PTH Revision Is Different Than PTH Parser', E_USER_ERROR);
+        
+        foreach (unpack(self::UNPACK, substr($file, 0, 16)) as $property => $value)
+            $this->$property = $value;
 
-		for ($Node = 0; $Node < $this->NumNodes; $Node++)
-			$this->Nodes[] = new Node(substr($file, 16 + ($Node * 40), 40));
+        for ($Node = 0; $Node < $this->NumNodes; $Node++)
+            $this->Nodes[] = new Node(substr($file, 16 + ($Node * 40), 40));
 
         $this->toPoly($this->polyRoad, 'Drive');
         $this->toPoly($this->polyLimit, 'Limit');
 
-		return $this;
-	}
+        if ($this->Custom)
+        {
+            $this->polyRoadBlocks = PolyGrid::fromArray($this->polyRoad, 16, 16, 1280);
+            $this->polyLimitBlocks = PolyGrid::fromArray($this->polyLimit, 16, 16, 1280);
+        }
+        
+        return $this;
+    }
     public function toPoly(array &$nodePolys, $limitRoad)
     {
         array_splice($nodePolys, 0, count($nodePolys));
@@ -84,109 +94,111 @@ class PTH
         }
         
         // Close the path
-        $nodePolys[] = new Polygon2D(array($pa, $pb, $nodePolys[0]->points[1], $nodePolys[0]->points[0]));
+        $nodePolys[] = new Polygon2D(array($pa, $pb, $nodePolys[0]->Points[1], $nodePolys[0]->Points[0]));
     }
-	public function isOnRoad($x, $y, $NodeID)
-	{
-	    $x /= 65536;
-	    $y /= 65536;
-	    
-	    // Check if point is within the left and right lines of the path
-        $p1 = $this->polyRoad[$NodeID]->points[1];
-        $p2 = $this->polyRoad[$NodeID]->points[2];
-        if (($y - $p1->y) * ($p2->x - $p1->x) - ($x - $p1->x) * ($p2->y - $p1->y) < 0)
-            return false;
-        
-        $p1 = $this->polyRoad[$NodeID]->points[3];
-        $p2 = $this->polyRoad[$NodeID]->points[0];
-        if (($y - $p1->y) * ($p2->x - $p1->x) - ($x - $p1->x) * ($p2->y - $p1->y) < 0)
-            return false;
+    public function isOnRoad($x, $y, &$NodeID)
+    {
+        return $this->isOnPath($x, $y, $NodeID, 'polyRoad');
+    }
+    public function isOnLimit($x, $y, &$NodeID)
+    {
+        return $this->isOnPath($x, $y, $NodeID, 'polyLimit');
+    }
+    public function isOnPath($x, $y, &$NodeID, $limitRoad)
+    {
+        $x /= 65536;
+        $y /= 65536;
+
+        if ($this->Custom)
+        {
+            // Get the quadrant-sorted portion of the poly array
+            $lr = $limitRoad.'Blocks';
+            $polyArray = array();
+            $numPolies = $this->$lr->getArrayByPoint(new Point2D($x, $y), $polyArray);
+            for ($i = 0; $i < $numPolies; $i++)
+            {
+                if ($polyArray[$i]->Poly->contains(new Point2D($x, $y)))
+                {
+                    $NodeID = $polyArray[$i]->Key;
+                    return true;
+                }
+            }
             
-        return true;
-	}
-	public function isOnLimit($x, $y, $NodeID)
-	{
-	    $x /= 65536;
-	    $y /= 65536;
-	    
-	    // Check if point is within the left and right lines of the path
-        $p1 = $this->polyLimit[$NodeID]->points[1];
-        $p2 = $this->polyLimit[$NodeID]->points[2];
-        if (($y - $p1->y) * ($p2->x - $p1->x) - ($x - $p1->x) * ($p2->y - $p1->y) < 0)
             return false;
-        
-        $p1 = $this->polyLimit[$NodeID]->points[3];
-        $p2 = $this->polyLimit[$NodeID]->points[0];
-        if (($y - $p1->y) * ($p2->x - $p1->x) - ($x - $p1->x) * ($p2->y - $p1->y) < 0)
-            return false;
+        }
+        else
+        {
+            $NodeID = ($NodeID < 0) ? 0 : $NodeID % $this->NumNodes;
             
-        return true;
-	}
-	public function drawPath ($fileName) {
-		$im = imagecreatetruecolor(2560, 2560);
-		$bg = imagecolorallocate($im, 254, 254, 254);
-		imagefill($im, 0, 0, $bg);
-		imagecolortransparent($im, $bg);
+            // Using regular nodeid to look up a polygon - $NodeID is used absolutely
+            // Check if point is within the left and right sides of the polygon
+            $p1 = $this->$limitRoad[$NodeID]->Points[1];
+            $p2 = $this->$limitRoad[$NodeID]->Points[2];
+            if (($y - $p1->y) * ($p2->x - $p1->x) - ($x - $p1->x) * ($p2->y - $p1->y) < 0)
+                return false;
+            
+            $p1 = $this->$limitRoad[$NodeID]->Points[3];
+            $p2 = $this->$limitRoad[$NodeID]->Points[0];
+            if (($y - $p1->y) * ($p2->x - $p1->x) - ($x - $p1->x) * ($p2->y - $p1->y) < 0)
+                return false;
+            
+            return true;
+        }
+            
+        return false;
+    }
+    public function drawPath($fileName)
+    {
+        $im = imagecreatetruecolor(2560, 2560);
+        $bgCol = imagecolorallocate($im, 254, 254, 254);
+        $pathCol = imagecolorallocatealpha($im, 64, 64, 64, 64);
+        imagefill($im, 0, 0, $bgCol);
+        imagecolortransparent($im, $bgCol);
 
-		$p =& $this;
+        for ($i = 0; $i < $this->NumNodes; $i++)
+        {
+            $pa = $this->polyRoad[$i]->Points[0];
+            $pb = $this->polyRoad[$i]->Points[1];
+            $pc = $this->polyRoad[$i]->Points[2];
+            $pd = $this->polyRoad[$i]->Points[3];
 
-		$LeftCos = cos (90 * M_PI / 180);
-		$LeftSin = sin (90 * M_PI / 180);
-		$RightCos = cos (-90 * M_PI / 180);
-		$RightSin = sin (-90 * M_PI / 180);
+            imagefilledpolygon($im,
+                               array($pa->x + 1280, -$pa->y + 1280, 
+                                     $pb->x + 1280, -$pb->y + 1280, 
+                                     $pc->x + 1280, -$pc->y + 1280, 
+                                     $pd->x + 1280, -$pd->y + 1280),
+                               4,
+                               $pathCol);
+        }
 
-		$i = $p->NumNodes - 1;
-		$dlx2 = ($p->Nodes[$i]->DirX * $LeftCos - (-$p->Nodes[$i]->DirY) * $LeftSin) * $p->Nodes[$i]->DriveLeft + ($p->Nodes[$i]->CenterX + 1024);
-		$dly2 = ((-$p->Nodes[$i]->DirY) * $LeftCos + $p->Nodes[$i]->DirX * $LeftSin) * $p->Nodes[$i]->DriveLeft + ((-$p->Nodes[$i]->CenterY) + 1024);
-		$drx2 = ($p->Nodes[$i]->DirX * $RightCos - (-$p->Nodes[$i]->DirY) * $RightSin) * -$p->Nodes[$i]->DriveRight + ($p->Nodes[$i]->CenterX + 1024);
-		$dry2 = ((-$p->Nodes[$i]->DirY) * $RightCos + $p->Nodes[$i]->DirX * $RightSin) * -$p->Nodes[$i]->DriveRight + ((-$p->Nodes[$i]->CenterY) + 1024);
-
-		$path_col = imagecolorallocatealpha($im, 64, 64, 64, 64);
-
-		for ($i = 0; $i < $p->NumNodes; $i++) {
-			$dlx = ($p->Nodes[$i]->DirX * $LeftCos - (-$p->Nodes[$i]->DirY) * $LeftSin) * $p->Nodes[$i]->DriveLeft + ($p->Nodes[$i]->CenterX + 1024);
-			$dly = ((-$p->Nodes[$i]->DirY) * $LeftCos + $p->Nodes[$i]->DirX * $LeftSin) * $p->Nodes[$i]->DriveLeft + ((-$p->Nodes[$i]->CenterY) + 1024);
-			$drx = ($p->Nodes[$i]->DirX * $RightCos - (-$p->Nodes[$i]->DirY) * $RightSin) * -$p->Nodes[$i]->DriveRight + ($p->Nodes[$i]->CenterX + 1024);
-			$dry = ((-$p->Nodes[$i]->DirY) * $RightCos + $p->Nodes[$i]->DirX * $RightSin) * -$p->Nodes[$i]->DriveRight + ((-$p->Nodes[$i]->CenterY) + 1024);
-
-			$p_array = array ($dlx2, $dly2, $dlx, $dly, $drx, $dry, $drx2, $dry2);
-
-			imagefilledpolygon($im, $p_array, 4, $path_col);
-
-			$dlx2 = $dlx;
-			$dly2 = $dly;
-			$drx2 = $drx;
-			$dry2 = $dry;
-		}
-
-		imagepng($im, $fileName);
-		imagedestroy($im);
-	}
+        imagepng($im, $fileName);
+        imagedestroy($im);
+    }
 }
 class Node
 {
-	const UNPACK = 'lCenterX/lCenterY/lCenterZ/fDirX/fDirY/fDirZ/fLimitLeft/fLimitRight/fDriveLeft/fDriveRight';
-	
-	public $CenterX = 0;
-	public $CenterY = 0;
-	public $CenterZ = 0;
-	public $DirX;
-	public $DirY;
-	public $DirZ;
-	public $LimitLeft;
-	public $LimitRight;
-	public $DriveLeft;
-	public $DriveRight;
+    const UNPACK = 'lCenterX/lCenterY/lCenterZ/fDirX/fDirY/fDirZ/fLimitLeft/fLimitRight/fDriveLeft/fDriveRight';
+    
+    public $CenterX = 0;
+    public $CenterY = 0;
+    public $CenterZ = 0;
+    public $DirX;
+    public $DirY;
+    public $DirZ;
+    public $LimitLeft;
+    public $LimitRight;
+    public $DriveLeft;
+    public $DriveRight;
 
-	public function __construct($RawNode)
-	{
-		foreach (unpack(self::UNPACK, $RawNode) as $property => $value)
-			$this->$property = $value;
+    public function __construct($RawNode)
+    {
+        foreach (unpack(self::UNPACK, $RawNode) as $property => $value)
+            $this->$property = $value;
 
-	    $this->CenterX /= 65536;
-	    $this->CenterY /= 65536;
-	    $this->CenterZ /= 65536;
-	    //$this->DirY = -$this->DirY;
-	}
+        $this->CenterX /= 65536;
+        $this->CenterY /= 65536;
+        $this->CenterZ /= 65536;
+        //$this->DirY = -$this->DirY;
+    }
 }
 ?>
