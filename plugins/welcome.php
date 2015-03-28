6e5cfd630bd8fa52db74cc03bbabff4744a00c9e
Macgyver H <T3charmy@gmail.com>
2015-03-28 17:04:08 -0500

    update WelcomePlugin to BetterButtonManager

--- plugins/welcome.php
+++ plugins/welcome.php
@@ -7,6 +7,8 @@ class welcome extends Plugins
 	const VERSION = PHPInSimMod::VERSION;
 	const DESCRIPTION = 'Welcome messages for clients, and Message of the Day (MOTD)';
 
+    protected $BTNMan = array();
+
 	public function __construct()
 	{
 		$this->registerPacket('onPrismConnect', ISP_VER);
@@ -22,20 +24,25 @@ class welcome extends Plugins
 	{
 		if ($NCN->UCID == 0)
 			return;
-		
-		$Title = new Button('poweredBy', Button::$TO_ALL);
-		$Title->Text('This server is powered by');
-		$Title->registerOnClick($this, 'onPoweredByClick');
-		$Title->T(IS_Y_MAX - IS_Y_MIN)->L(IS_X_MIN)->W(IS_X_MAX)->H(8)->send();
-		
-		$Msg = new Button('prism', Button::$TO_ALL);
-		$Msg->Text('^3PRISM ^8Version ^7'.PHPInSimMod::VERSION.'^8.');
-		$Msg->T(IS_Y_MAX - IS_Y_MIN + 8)->L(IS_X_MIN)->W(IS_X_MAX)->H(8)->send();
+
+		$this->BTNMan[$NCN->UName] = new betterButtonManager($NCN->UCID);
+        $this->createNamedTimer("WelcomePlugin_ButtonHandle_{$NCN->UName}", 'DoButtonHandle', 0.50, Timer::REPEAT, array($NCN->UName));
+
+        $this->BTNMan[$UName]->InitButton('poweredBy', 'welcomeMSG', (IS_Y_MAX - IS_Y_MIN), IS_X_MIN, IS_X_MAX, 8, ISB_DARK, 'This server is powered by', 10);
+        $this->BTNMan[$UName]->AddClickEventToBtn('poweredBy', $this, 'onPoweredByClick', array($NCN->UCID))
+        $this->BTNMan[$UName]->InitButton('prism', 'welcomeMSG', (IS_Y_MAX - IS_Y_MIN + 8), IS_X_MIN, IS_X_MAX, 8, ISB_DARK, '^3PRISM ^8Version ^7'.PHPInSimMod::VERSION.'^8.', 10);
+
 	}
 	
-	public function onPoweredByClick(IS_BTC $BTC)
+	public function onPoweredByClick($UCID)
 	{
-		echo 'Button clicked! ' . $BTC;
+		IS_MTC()->Text('^3For more info on PRISM visit:')->UCID($UCID)->send();
+		IS_MTC()->Text('^7https://www.lfs.net/forum/312')->UCID($UCID)->send();
 	}
+
+    public function DoButtonHandle($UName)
+    {
+        $this->BTNMan[$UName]->HandleButtons();
+    }
 }
 ?>
