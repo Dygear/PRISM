b9f91d301bdffec2da9d69c4f698664db2007154
Jay Looney <jay.m.looney@gmail.com>
2015-04-03 01:48:00 -0500

    fixed improper usage of 'use' keyword for anonymous functions, and set function to a callable

--- modules/prism_functions.php
+++ modules/prism_functions.php
@@ -49,10 +49,10 @@ function isDirInDir($path1, $path2)
     foreach ($p1 as $index => $part)
     {
         if ($part === '') {
-            continue; 
+            continue;
         }
         if (!isset($p2[$index]) || $part != $p2[$index]) {
-            return false; 
+            return false;
         }
     }
 
@@ -77,7 +77,7 @@ function findPHPLocation($windows = false)
             chdir('/');
             $exp = explode("\r\n", shell_exec('dir /s /b php.exe'));
             if (preg_match('/^.*\\\php\.exe$/', $exp[0])) {
-                $phpLocation = $exp[0]; 
+                $phpLocation = $exp[0];
             }
             chdir(ROOTPATH);
         }
@@ -87,11 +87,11 @@ function findPHPLocation($windows = false)
         $exp = explode(' ', shell_exec('whereis php'));
         $count = count($exp);
         if ($count == 1) {                // Some *nix's output is only the path
-            $phpLocation = $exp[0]; 
+            $phpLocation = $exp[0];
         }
         else if ($count > 1) {            // FreeBSD for example has more info on the line, like :
             $phpLocation = $exp[1];        // php: /user/local/bin/php /usr/local/man/man1/php.1.gz
-        }    
+        }
     }
 
     return $phpLocation;
@@ -102,7 +102,7 @@ function validatePHPFile($file)
     // Validate script
     $fileContents = file_get_contents($file);
     if (!eval('return true;'.preg_replace(array('/^<\?(php)?/', '/\?>$/'), '', $fileContents))) {
-        return array(false, array('Errors parsing '.$file)); 
+        return array(false, array('Errors parsing '.$file));
     }
 
     // Validate any require_once or include_once files.
@@ -124,7 +124,7 @@ function flagsToInteger($flagsString = '')
 {
     // We don't have anything to parse.
     if ($flagsString == '') {
-        return false; 
+        return false;
     }
 
     $flagsBitwise = 0;
@@ -135,12 +135,12 @@ function flagsToInteger($flagsString = '')
 
         // We only want a (ASCII = 97) through z (ASCII 122), nothing else.
         if ($char < 97 || $char > 122) {
-            continue; 
+            continue;
         }
 
         // Check we have already set that flag, if so skip it!
         if ($flagsBitwise & (1 << ($char - 97))) {
-            continue; 
+            continue;
         }
 
         // Add the value to our $flagBitwise intager.
@@ -153,7 +153,7 @@ function flagsToString($flagsBitwise = 0)
 {
     $flagsString = '';
     if ($flagsBitwise == 0) {
-        return $flagsString; 
+        return $flagsString;
     }
 
     // This makes sure we only handle the flags we know by unsetting any unknown bits.
@@ -161,7 +161,7 @@ function flagsToString($flagsBitwise = 0)
 
     // Converts bits to the char forms.
     for ($i = 0; $i < 26; ++$i) {
-        $flagsString .= ($flagsBitwise & (1 << $i)) ? chr($i + 97) : null; 
+        $flagsString .= ($flagsBitwise & (1 << $i)) ? chr($i + 97) : null;
     }
 
     return $flagsString;
@@ -203,7 +203,7 @@ function ucwordsByChar($string, $delimiter)
     foreach (explode($delimiter, $string) as $k => $v)
     {
         if ($k > 0) {
-            $out .= $delimiter; 
+            $out .= $delimiter;
         }
         $out .= ucfirst($v);
     }
@@ -213,13 +213,13 @@ function ucwordsByChar($string, $delimiter)
 function getIP(&$ip)
 {
     if (verifyIP($ip)) {
-        return $ip; 
+        return $ip;
     }
     else
     {
         $tmp_ip = @gethostbyname($ip);
         if (verifyIP($tmp_ip)) {
-            return $tmp_ip; 
+            return $tmp_ip;
         }
     }
 
@@ -254,30 +254,32 @@ function timeToStr($time, $fraction=1000)
 
 function sortByKey($key)
 {
-    return function ($left, $right) {
-        use ($key)
-    
+    $sort = function ($left, $right) use ($key) {
+
         if ($left[$key] == $right[$key]) {
-            return 0; 
+            return 0;
         }
         else {
-            return ($left[$key] < $right[$key]) ? -1 : 1; 
+            return ($left[$key] < $right[$key]) ? -1 : 1;
         }
     };
+
+    return $sort;
 }
 
 function sortByProperty($property)
 {
-    return function ($left, $right) {
-        use ($property)
-    
+    $sort = function ($left, $right) use ($property) {
+
         if ($left->$property == $right->$property) {
-            return 0; 
+            return 0;
         }
         else {
-            return ($left->$property < $right->$property) ? -1 : 1; 
+            return ($left->$property < $right->$property) ? -1 : 1;
         }
     };
+
+    return $sort;
 }
 
 class Msg2Lfs
@@ -296,14 +298,14 @@ class Msg2Lfs
     public function &__call($name, array $arguments)
     {
         if (property_exists(get_class($this), $name)) {
-            $this->$name = array_shift($arguments); 
+            $this->$name = array_shift($arguments);
         }
         return $this;
     }
 
     public function send($hostId = null)
     {
-        if ($this->Text == '') { return; 
+        if ($this->Text == '') { return;
         }
 
         global $PRISM;
@@ -317,20 +319,20 @@ class Msg2Lfs
         {
             // Multi player
             if ($this->PLID > 0) {
-                IS_MTC()->PLID($this->PLID)->Text($this->Text)->Sound($this->Sound)->send(); 
+                IS_MTC()->PLID($this->PLID)->Text($this->Text)->Sound($this->Sound)->send();
             }
             else if ($this->UCID > 0) {
-                IS_MTC()->UCID($this->UCID)->Text($this->Text)->Sound($this->Sound)->send(); 
+                IS_MTC()->UCID($this->UCID)->Text($this->Text)->Sound($this->Sound)->send();
             }
             else {
-                IS_MSX()->Msg($this->Text)->send(); 
+                IS_MSX()->Msg($this->Text)->send();
             }
         }
 
         return $this;
     }
-}; function Msg2Lfs() 
+}; function Msg2Lfs()
 {
-    return new Msg2Lfs; 
+    return new Msg2Lfs;
 }
 ?>
