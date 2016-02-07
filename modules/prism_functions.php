692730ef48f8434ba6d60b90e1859f6ec926e762
Dave LB <nl2dav@gmail.com>
2016-02-07 05:24:56 +0100

    Update prism_functions.php
    I got annoyed that everything is black and white in the console. ANSI colors help a bit to color some stuff to see whats going on.

--- modules/prism_functions.php
+++ modules/prism_functions.php
@@ -1,10 +1,41 @@
 <?php
 
-function console($line, $EOL = true)
+function console($line, $EOL = true, $fcolor = "white", $bcolor = "black")
 {
+    // example:
+    // console("this is my exampletext", true, "light_green", "black");
+
+    $ansi_fcolor_array = array( "black" => "0;30", 
+                                "dark_gray" => "1;30",
+                                "red" => "0;31",
+                                "light_red" => "1;31",  
+                                "green" => "0;32",
+                                "light_green" => "1;32", 
+                                "brown" => "0;33",
+                                "yellow" => "1;33",                                                                                              
+                                "blue" => "0;34",                                 
+                                "light_blue" => "1;34",
+                                "purple" => "0;35",
+                                "light_purple" => "1;35",                                  
+                                "cyan" => "0;36",
+                                "light_cyan" => "1;36",
+                                "light_gray" => "0;37",
+                                "white" => "1;37"
+                              );
+
+    $ansi_bcolor_array = array( "black" => "40",
+                                "red" => "41",
+                                "green" => "42",
+                                "yellow" => "43",
+                                "blue" => "44",
+                                "magenta" => "45",
+                                "cyan" => "46",
+                                "light_gray" => "47"
+                            );
+
     // Add log to file
     // Effected by PRISM_LOG_MODE && PRISM_LOG_FILE_MODE
-    echo $line . (($EOL) ? PHP_EOL : '');
+    echo "\033[".$ansi_fcolor_array[$fcolor]."m\033[".$ansi_bcolor_array[$bcolor]."m".$line. "\033[0m" . (($EOL) ? PHP_EOL : '');
 }
 
 function translateEngine($lang_subdirectory, $languageID, $messageID, $args = array(), $fallback = 'en')
